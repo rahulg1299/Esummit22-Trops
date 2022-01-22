74e137d1bd23b0158b1dc29aeffc5bfe2a532306
Shubham Agrawal <agrawal152000@gmailcom>
2022-01-22 11:24:14 +0530

    minor change

--- Backend/routes/landingAPIs.js
+++ Backend/routes/landingAPIs.js
@@ -11,15 +11,14 @@ router.get('/equipment',(req,res) =>{
     const data = {}
 
     Promise.all([
-        db.EquipmentType.findAll({limit : 5}),
         db.Equipment.findAll({where : {toBuy : true} , limit : 5 }),
         db.Equipment.findAll({where : {toBuy :false} , limit : 5 }),
     ]).then(value => {
-        data.equipmentTypes = value[0]
-        data.toBuyEquipments = value[1]
-        data.toRentEquipments = value[2]
+        data.toBuyEquipments = value[0]
+        data.toRentEquipments = value[1]
         res.send(data);
     }).catch(err => console.log(err));
+    
 })
 
 
